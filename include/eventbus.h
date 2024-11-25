bab8971094e089ebc49ab97501b4ba1229f8e2e9
pavver <pavvers1@gmail.com>
2024-11-25 22:16:28 +0200

    more bus functions

--- include/eventbus.h
+++ include/eventbus.h
@@ -8,19 +8,39 @@
 
 typedef struct EventBus EventBus;
 
-// Створення EventBus
-EventBus *eventbus_create(void);
+typedef struct eventbus_config_t
+{
+  uint8_t max_subscribers;
+  uint8_t events_queue_len;
+} eventbus_config_t;
+
+#define EVENTBUS_DEFAULT_CONFIG ((struct eventbus_configure_t){ \
+    .max_subscribers = 20,                                      \
+    .events_queue_len = 20,                                     \
+})
+
+EventBus *eventbus_create();
+EventBus *eventbus_create(eventbus_config_t config);
 
 // Знищення EventBus
 void eventbus_destroy(EventBus *bus);
 
 // Реєстрація підписників
-int eventbus_register(EventBus *bus, Subscriber *subscriber);
+int eventbus_subscribe(EventBus *bus, Subscriber *subscriber);
 
-int eventbus_unregister(EventBus *bus, Subscriber *subscriber);
+int eventbus_unsubscribe(EventBus *bus, Subscriber *subscriber);
+
+Event *eventbus_nextEvent(EventBus *bus, Subscriber *subscriber);
 
 // Відправлення події
-int eventbus_publish(EventBus *bus, Event *event);
-int eventbus_request(EventBus *bus, Event *request, void *response, size_t response_size);
+int eventbus_publish_message(EventBus *bus, EventType event_type);
+int eventbus_publish_message(EventBus *bus, EventType event_type, EventInputData *input_data);
+
+int eventbus_publish_request(EventBus *bus, EventType event_type);
+int eventbus_publish_request(EventBus *bus, EventType event_type, EventInputData *input_data);
+int eventbus_publish_request(EventBus *bus, EventType event_type, EventInputData *input_data, EventOutputCallbackData *callback);
+
+bool eventbus_request_existResponseData(EventBus *bus, Subscriber *subscriber, Event *event);
+EventData *eventbus_awaitData(EventBus *bus, Subscriber *subscriber, Event *event);
 
 #endif // EVENTBUS_H
