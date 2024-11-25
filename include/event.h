e7256766a527afee85bf6d0f234720a82674a9aa
pavver <pavvers1@gmail.com>
2024-11-25 11:20:13 +0200

    event type

--- include/event.h
+++ include/event.h
@@ -43,38 +43,65 @@ typedef int (*EventDataReadFn)(void *buffer, size_t size);
 
 /**
  * @brief Функція відправки данних події паблішеру
+ * @param context - контекст паблішера
  * @param buffer - данні для відправки
  * @param size - розмір данних
+ * @param event_status - стан події, якщо EVENT_WAIT_REQUEST то це не останні данні,
+ * під час обробки цієї події може бути ще виклик цієї функції. якщо EVENT_DONE, то це останні данні і обробка події повністью завершена.
  * @return статус відправки данних, -1 у разі помилки
  */
-typedef int (*EventDataWriteFn)(void *buffer, size_t size);
+typedef int (*EventDataWriteFn)(void *context, void *buffer, size_t size, EventStatus event_status);
 
-enum event_status : uint8_t
+/**
+ * @brief Тип події
+ */
+typedef struct EventType
 {
   /**
-   * @brief Очікує виклик від підписника
+   * @brief Група події
    */
-  EventWaitInvoke,
+  uint8_t group;
 
-  /// @brief Подія в обробці підписником
-  EventInWork,
+  /**
+   * @brief Тип події
+   */
+  uint8_t type;
+} EventType;
 
-  // Подія виконана, але подія є запитом і очікує обробки відповіді паблішером
-  EventRequestWait,
+/**
+ * @brief Будь яка група подій
+ */
+#define EVENT_GROUP_ANY 0
 
-  /// @brief Подія виконана
-  EventDone,
-};
+/**
+ * @brief Будь який тип події
+ */
+#define EVENT_TYPE_ANY 0
 
-struct event_type
+/**
+ * @brief Конкретна подія з конкретної групи
+ * @param group Група події
+ * @param type Тип події
+ */
+#define EVENT_TYPE(group, type) ((EventType){.group = type, .type = type})
+
+/**
+ * @brief Будь яка подія з конкретної групи
+ * @param group Група події
+ */
+#define EVENT_TYPE(group) ((EventType){.group = group, .type = EVENT_TYPE_ANY})
+
+/**
+ * @brief Будь яка подія
+ */
+#define EVENT_GROUP_ANY() ((EventType){.group = EVENT_GROUP_ANY, .type = EVENT_TYPE_ANY})
+
+/**
+ * @brief Структура яка містить дані для відправки підписнику, та/або функцію яка дозволить прочитати вхідні данні
+ */
+struct EventInputData
 {
-  uint8_t base_type;
-  uint8_t subtype;
-};
-
-struct event_message_data
-{
-  EventDataWriteFn write_fn;
+  EventDataReadFn read_fn;
   void *data;
   size_t data_size;
 };
